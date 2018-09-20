de9d801194090386b74bbafebc84acd90921f8e1
もっへもへ <mohemohe@ghippos.net>
2018-09-21 01:03:11 +0900

    改善

--- scripts/ikku.js
+++ scripts/ikku.js
@@ -1,28 +1,37 @@
 const Haiku = require('@mohemohe/haiku.js').default;
 
 module.exports = (robot) => {
-  robot.hear(/^\/ikku\s?(.*)$/mi, (msg) => {
-    let status = msg.match[1];
-    switch (status) {
+  robot.hear(/^\/ikku(.*)$/mi, (msg) => {
+    const command = msg.match[1].trim();
+    const status = robot.brain.get(`kokoroio_ikku_enable_${msg.envelope.room}`);
+    switch (command) {
       case 'enable':
-        robot.brain.set(`kokoroio_ikku_enable_${msg.envelope.room}`, 1);
-        robot.brain.save();
-        msg.send('このルームの川柳検出を有効にしました');
+        if (status === 1) {
+          msg.send('このチャンネルの川柳検出は既に有効です');
+        } else {
+          robot.brain.set(`kokoroio_ikku_enable_${msg.envelope.room}`, 1);
+          robot.brain.save();
+          msg.send('このチャンネルの川柳検出を有効にしました');
+        }
         break;
       case 'disable':
-        robot.brain.set(`kokoroio_ikku_enable_${msg.envelope.room}`, 0);
-        robot.brain.save();
-        msg.send('このルームの川柳検出を無効にしました');
+        if (status === 1) {
+          robot.brain.set(`kokoroio_ikku_enable_${msg.envelope.room}`, 0);
+          robot.brain.save();
+          msg.send('このチャンネルの川柳検出を無効にしました');
+        } else {
+          msg.send('このチャンネルの川柳検出は既に無効です');
+        }
         break;
       case 'status':
-        status = robot.brain.get(`kokoroio_ikku_enable_${msg.envelope.room}`);
         if (status === 1) {
-          msg.send('川柳検出は有効です');
+          msg.send('このチャンネルの川柳検出は有効です');
         } else {
-          msg.send('川柳検出は無効です');
+          msg.send('このチャンネルの川柳検出は無効です');
         }
         break;
       default:
+        msg.send('/ikku [enable|disable|status]');
         break;
     }
   });
