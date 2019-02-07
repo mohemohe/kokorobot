70a7c87f96b146a9067b5f8bfb6e19e4caabe906
mohemohe <mohemohe@ghippos.net>
2019-02-07 12:38:08 +0900

    hi

--- scripts/mstdn.js
+++ scripts/mstdn.js
@@ -134,7 +134,8 @@ class Mstdn {
     const acct = this.robot.brain.get(`kokoroio_mstdn_${msg.message.room}`) || {};
     msg.reply(`
 
-\`\`\`${
+\`\`\`
+${
       Object.keys(acct).map(screenName => {
         // FIXME: 何度も使ってる気がするから切り出したほうが良さげ
         let mode = Mode.ALL;
@@ -146,7 +147,8 @@ class Mstdn {
 
         return `${Mstdn.unescape(screenName)}: ${mode}`;
       }).join('\n')
-}\`\`\``);
+}
+\`\`\``);
   }
 
   reconnect(msg) {
