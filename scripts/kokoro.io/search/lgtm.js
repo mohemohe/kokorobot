a11f8b8cc33604ebc9e79415b26e757d15836c01
mohemohe <mohemohe@ghippos.net>
2019-03-31 04:10:45 +0900

    close #9

--- scripts/kokoro.io/search/lgtm.js
+++ scripts/kokoro.io/search/lgtm.js
@@ -9,12 +9,13 @@ module.exports = (robot) => {
     }
 
     try {
-      const response = await axios.get('http://lgtm.in/g', {
+      // NOTE: https://github.com/lgtmapp/lgtmapp-web-extension/blob/master/app/scripts/components/App.js#L45
+      const response = await axios.get('https://www.lgtm.app/api/images/random', {
         headers: {
           Accept: 'application/json',
         },
       });
-      msg.send(response.body.data.actualImageUrl);
+      msg.send(response.data.imageURL);
     } catch (e) {
       console.log(e);
       msg.send('APIエラーが発生しました');
