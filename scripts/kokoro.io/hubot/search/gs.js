5dae8fc9032a05f89c5b2f2f7b50dfc561078741
mohemohe <mohemohe@ghippos.net>
2019-05-30 00:16:18 +0900

    未使用コードを削除

--- scripts/kokoro.io/hubot/search/gs.js
+++ scripts/kokoro.io/hubot/search/gs.js
@@ -1,15 +1,5 @@
 const Prefix = require('../../helpers/prefix');
-const GoogleImages = require('google-images');
 const allowCommand = require('../../helpers/allowcommand');
-const random = require('../../helpers/random');
-
-function checkGoogle(msg) {
-  if (process.env.GOOGLE_CSE_ID && process.env.GOOGLE_API_KEY) {
-    return new GoogleImages(process.env.GOOGLE_CSE_ID, process.env.GOOGLE_API_KEY);
-  }
-  msg.send('Google Custom Search APIキーが設定されていません\n環境変数 GOOGLE_CSE_ID にIDを、 GOOGLE_API_KEY にAPIキーをそれぞれ設定してください');
-  return null;
-}
 
 module.exports = (robot) => {
   robot.hear(Prefix.regex('/ifl (.*)/mi'), (msg) => {
